b163c352be70f8fd07f43c4798ddc9f7633d33bb
obednarc <Mazury12*>
2020-03-29 11:11:17 +0200

    Dodanie testów

---
+++ src/main/java/pl/edu/wszib/jwd/project/dao/SelectedColorDao.java
@@ -0,0 +1,10 @@
+package pl.edu.wszib.jwd.project.dao;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import pl.edu.wszib.jwd.project.model.SelectedColor;
+
+@Repository
+public interface SelectedColorDao extends CrudRepository<SelectedColor, Integer> {
+    SelectedColor findByColor(String color);
+}
